8102565de4f7fd750e5ca7d9db4c539d2878f656
Marc <marc.horlacher@gmail.com>
2019-12-04 19:26:11 +0100

    Added Jupyter-R-Notebook

---
+++ Jupyter-R-Notebook/init.r/DESeq-source.R
@@ -0,0 +1,24 @@
+## Set CRAN repository
+options(repos = "http://cran.r-project.org")
+
+# Bioconductor
+install.packages("BiocManager")
+BiocManager::install(version = "3.10")
+
+# DESeq2
+BiocManager::install("airway")
+BiocManager::install("tximeta")
+BiocManager::install("DESeq2")
+
+# Plotting
+install.packages("ggplot2")
+install.packages("dplyr")
+install.packages("pheatmap")
+install.packages("RColorBrewer")
+install.packages("ggbeeswarm")
+
+# Misc
+install.packages("readr")
+install.packages("PoiClaClu")
+install.packages("glmpca")
+install.packages("apeglm")
